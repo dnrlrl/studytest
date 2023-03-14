5c5f496f47763716f94da158b45aa175d85af732
sangwonlee <sangwonlee@nts-corp.com>
2023-03-14 18:38:13 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './index.module.scss'
+
+const Index = () => {
+  return (
+    <>
+    <div className={styles.dd}>
+      <div className={styles.aa}>ddd</div>
+    </div>
+    </>
+  )
+}
+
+export default Index;
